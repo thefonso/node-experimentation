fac8d626c59ed6506513c7ed68490b350ebeade2
thefonso <thefonso@thefonso.com>
2019-03-03 13:17:41 -0500

    add lead show edit and post routes

--- controllers/landing.js
+++ controllers/landing.js
@@ -9,6 +9,42 @@ exports.submit_lead = function(req, res, next) {
     return models.Lead.create({
         email: req.body.lead_email
     }).then(lead => {
-        res.redirect('/');
-    });
+        res.redirect('/leads');
+    })
+};
+
+exports.show_leads = function(req, res, next) {
+    return models.Lead.findAll().then(leads => {
+        res.render('landing', {title: 'Express', leads: leads });
+    })
+};
+
+exports.show_lead = function(req, res, next) {
+    return models.Lead.findOne({
+        where : {id:req.params.lead_id}
+    }).then(lead => {
+        res.render('lead', { lead : lead })
+    })
+};
+
+exports.show_edit_lead = function(req, res, next) {
+    return models.Lead.findOne({
+        where : {id:req.params.lead_id}
+    }).then(lead => {
+        res.render('lead/edit_lead', { lead : lead })
+    })
+};
+
+
+exports.edit_lead = function(req, res, next) {
+    req.params.lead_id
+    req.body.lead_email
+
+    return models.Lead.update({
+        email: req.lead_email
+    }, {
+        where: {id: req.params.lead_id}
+    }).then(result => {
+        res.redirect('/lead' + req.params.lead_id);
+    })
 };
